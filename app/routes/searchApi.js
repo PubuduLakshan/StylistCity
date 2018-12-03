786933fe9d74f975baa6184c1f5a259cd7567cdd
3LOGIXS\pubudu_k <pubudu.kulasinghe@gmail.com>
2018-12-03 18:21:43 +0530

    working filter search

--- app/routes/searchApi.js
+++ app/routes/searchApi.js
@@ -3,10 +3,21 @@ module.exports= function(app,db){
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('/search', (req, res) => {
-    
+    const sendObj = req.body.searchedObj;
+    console.log(sendObj);
     db.Stylist.findAll().then( (result) => res.json(result) )
     
 
-})
+});
+
+app.post('/search', (req, res) => {
+    const sendObj = req.body.searchedObj;
+    db.Stylist.findAll({
+        where :{city:sendObj.location}
+    }
+    ).then( (result) => res.json(result) ); 
+});
+
+
 }   
     
