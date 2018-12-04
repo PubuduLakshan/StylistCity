affb9c3cc191f19f936dfe64045998ddec3e620e
3LOGIXS\pubudu_k <pubudu.kulasinghe@gmail.com>
2018-12-04 15:11:36 +0530

    working api

--- app/routes/searchApi.js
+++ app/routes/searchApi.js
@@ -1,21 +1,30 @@
 
+const Op = require('sequelize').Op;
+
 module.exports= function(app,db){
+
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('/search', (req, res) => {
     const sendObj = req.body.searchedObj;
     console.log(sendObj);
     db.Stylist.findAll().then( (result) => res.json(result) )
-    
-
 });
 
 app.post('/search', (req, res) => {
     const sendObj = req.body.searchedObj;
+
     db.Stylist.findAll({
-        where :{city:sendObj.location}
+     where :{city:sendObj.location,type: sendObj.category, country:sendObj.gender}
+     /* where: {
+      [Op.or]: [{city: sendObj.location}, {type: sendObj.category },{}]
+       }*/
     }
-    ).then( (result) => res.json(result) ); 
+    ).then( (result) => {
+        if(result!== null){res.json(result)}
+        else{res.send("Result Not Found")}
+        } 
+        ); 
 });
 
 
